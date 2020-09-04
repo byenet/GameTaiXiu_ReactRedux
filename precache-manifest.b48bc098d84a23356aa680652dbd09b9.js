566e2ad7bf8afd6aac0337c72c795d6a2aba2901
byenet <nhathuynguyenho@gmail.com>
2020-09-03 22:24:42 -0700

    Updates

---
+++ precache-manifest.b48bc098d84a23356aa680652dbd09b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039f4c299c83c723bfa12ede9c64f663",
+    "url": "/GameTaiXiu_ReactRedux/index.html"
+  },
+  {
+    "revision": "02026b08c5c3f6098e63",
+    "url": "/GameTaiXiu_ReactRedux/static/css/2.8ab43a4e.chunk.css"
+  },
+  {
+    "revision": "2055dab46d0f478c8263",
+    "url": "/GameTaiXiu_ReactRedux/static/css/main.50ad1b98.chunk.css"
+  },
+  {
+    "revision": "02026b08c5c3f6098e63",
+    "url": "/GameTaiXiu_ReactRedux/static/js/2.e557bb33.chunk.js"
+  },
+  {
+    "revision": "a7b079a06d99e0bdddae2cf2b771ca7a",
+    "url": "/GameTaiXiu_ReactRedux/static/js/2.e557bb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2055dab46d0f478c8263",
+    "url": "/GameTaiXiu_ReactRedux/static/js/main.67741240.chunk.js"
+  },
+  {
+    "revision": "d95f24aa37a94942976d",
+    "url": "/GameTaiXiu_ReactRedux/static/js/runtime-main.7918e01e.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/GameTaiXiu_ReactRedux/static/media/Pony.1716f3f8.ttf"
+  },
+  {
+    "revision": "cddc35618404287a6a5b115b732d3f34",
+    "url": "/GameTaiXiu_ReactRedux/static/media/bgGame.cddc3561.png"
+  }
+]);
